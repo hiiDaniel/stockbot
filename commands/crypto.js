e6308c6cc76ddb44a4b9807d5b980ca16eaf89be
Jeremiah Wuenschel <jeremiah.wuenschel@gmail.com>
2020-12-30 22:44:58 -0800

    Initial crypto and stock js

---
+++ commands/crypto.js
@@ -0,0 +1,62 @@
+const discord = require('discord.js');
+const axios = require('axios');
+
+module.exports.run = async (bot, message, args) => {
+
+    let baseURL = 'https://data.messari.io/api/v1/assets/';
+
+    if (!args.length) {
+        return message.channel.send(`You didn't provide a crypto symbol, ${message.author}!`);
+    }
+    else if (args.length) {
+
+        let getData = async () => {
+            let url = await axios.get(baseURL + args + '/metrics'); //crypto data
+            let cryptoInfo = url.data;
+
+            return {
+                cryptoInfo
+            };            
+        }
+
+        let cryptoData = await getData();
+        let cryptoAtt = cryptoData.cryptoInfo.data;
+
+        message.channel.send( {
+            embed: {
+                color: 0xebc11a ,
+                title: cryptoAtt.name + ' (' + cryptoAtt.symbol + ')',
+                fields:[
+                    {
+                    name: "Price",
+                    value:  `$ ${cryptoAtt.market_data.price_usd}`|| "N/A",
+                    inline: true
+                    },
+                    {
+                    name: "\u200B",
+                    value: "\u200B",
+                    inline: true
+                    },
+                    {
+                    name: "24h Change",
+                    value:  `${cryptoAtt.market_data.percent_change_usd_last_24_hours.toFixed(2)}%`|| "N/A",
+                    inline: true
+                    }
+                ],
+                timestamp: new Date()
+
+            }}).then(async getChange => {
+            if (cryptoAtt.market_data.percent_change_usd_last_24_hours > 0) {
+                await getChange.react('📈');
+            }
+            else {
+                await getChange.react('📉');
+            }
+        });
+    }
+
+};
+
+module.exports.help = {
+    name: "crypto"
+};
---
+++ commands/stocks.js
@@ -0,0 +1,177 @@
+const discord = require('discord.js');
+const axios = require('axios');
+
+module.exports.run = async (bot, message, args) => {
+
+    let baseURL = 'https://cloud.iexapis.com/stable/stock/';
+    let token = '';
+
+        if (!args.length) {
+            return message.channel.send(`You didn't provide a stock symbol, ${message.author}!`);
+        }
+        else if (args.length) {
+
+            let getCompany = async () => {
+                let stockCompany = await axios.get(baseURL + args + '/batch?types=company,quote,book,logo,stats,news,chart&range=1m&last=1' + token); //company data
+                let companyInfo = stockCompany.data;
+
+                return {
+                    companyInfo
+                };
+            }
+
+            let stockInfo = await getCompany();
+            let newsLink = stockInfo.companyInfo.news[0].summary.substring(0, 200) + "...";
+
+            message.channel.send( {
+                embed: {
+                    color: 0x0de5de,
+                    title: stockInfo.companyInfo.company.companyName + ' (' + stockInfo.companyInfo.company.symbol + ')',
+                    url: stockInfo.companyInfo.company.website,
+                    description: stockInfo.companyInfo.company.description,
+                    thumbnail: {
+                        url: stockInfo.companyInfo.logo.url
+                    },
+                    fields:[
+                        {
+                        name: "$" + stockInfo.companyInfo.quote.latestPrice || "N/A",
+                        value: stockInfo.companyInfo.quote.change + " (" + (stockInfo.companyInfo.quote.changePercent * 100).toFixed(2) + "%)" || "N/A",
+                        inline: false
+                        },
+                        {
+                        name: "\u200B",
+                        value: "\u200B",
+                        inline: false
+                        },
+                        {
+                        name: "Exchange",
+                        value: stockInfo.companyInfo.company.exchange || "N/A",
+                        inline: true
+                        },
+                        {
+                        name: "\u200B",
+                        value: "\u200B",
+                        inline: true
+                        },
+                        {
+                        name: "Market Cap",
+                        value: (stockInfo.companyInfo.quote.marketCap).toLocaleString('en') || "N/A",
+                        inline: true
+                        },
+                        {
+                        name: "Sector",
+                        value: stockInfo.companyInfo.company.sector || "N/A",
+                        inline: true
+                        },
+                        {
+                        name: "\u200B",
+                        value: "\u200B",
+                        inline: true
+                        },
+                        {
+                        name: "P/E Ratio",
+                        value: stockInfo.companyInfo.quote.peRatio || "N/A",
+                        inline: true
+                        },
+                        {
+                        name: "Industry",
+                        value: stockInfo.companyInfo.company.industry || "N/A",
+                        inline: true
+                        },
+                        {
+                        name: "\u200B",
+                        value: "\u200B",
+                        inline: true
+                        },
+                        {
+                        name: "Earnings Per Share (EPS)",
+                        value: "$" + stockInfo.companyInfo.stats.ttmEPS || "N/A",
+                        inline: true
+                        },
+                        {
+                        name: "Today's High/Low",
+                        value: "$" + stockInfo.companyInfo.chart[0].high + " / " + "$" + stockInfo.companyInfo.chart[0].low || "N/A",
+                        inline: true
+                        },
+                        {
+                        name: "\u200B",
+                        value: "\u200B",
+                        inline: true
+                        },
+                        {
+                        name: "Annualized Dividend",
+                        value: "$" + (stockInfo.companyInfo.stats.ttmDividendRate).toFixed(2) || "N/A",
+                        inline: true
+                        },
+                        {
+                        name: "Share Volume",
+                        value: (stockInfo.companyInfo.chart[0].volume).toLocaleString('en') || "N/A",
+                        inline: true
+                        },
+                        {
+                        name: "\u200B",
+                        value: "\u200B",
+                        inline: true
+                        },
+                        {
+                        name: "Ex Dividend Date",
+                        value: stockInfo.companyInfo.stats.exDividendDate || "N/A",
+                        inline: true
+                        },
+                        {
+                        name: "Average Volume",
+                        value: (stockInfo.companyInfo.quote.avgTotalVolume).toLocaleString('en') || "N/A",
+                        inline: true
+                        },
+                        {
+                        name: "\u200B",
+                        value: "\u200B",
+                        inline: true
+                        },
+                        {
+                        name: "Previous Close",
+                        value: "$" + stockInfo.companyInfo.quote.previousClose || "N/A",
+                        inline: true
+                        },
+                        {
+                        name: "Current Yield",
+                        value: (stockInfo.companyInfo.stats.dividendYield * 100).toFixed(2) + "%" || "N/A",
+                        inline: true
+                        },
+                        {
+                        name: "\u200B",
+                        value: "\u200B",
+                        inline: true
+                        },
+                        {
+                        name: "52 Week High/Low",
+                        value: "$" + stockInfo.companyInfo.stats.week52high + " / " + "$" + stockInfo.companyInfo.stats.week52low || "N/A",
+                        inline: true
+                        },
+                        {
+                        name: "\u200B",
+                        value: "\u200B",
+                        inline: false
+                        },
+                        {
+                        name: stockInfo.companyInfo.news[0].headline || "N/A",
+                        value: `${newsLink} [read more](${stockInfo.companyInfo.news[0].url})`,
+                        inline: false
+                        }
+                    ],
+                    timestamp: new Date()
+
+            }}).then(async getChange => {
+                if (stockInfo.companyInfo.quote.change > 0) {
+                    await getChange.react('📈');
+                }
+                else {
+                    await getChange.react('📉');
+                }
+            });
+        }
+};
+
+module.exports.help = {
+    name: "stock"
+};
