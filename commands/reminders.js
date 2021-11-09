0106240cfb86f4903cef35e85ee2705633099b3d
Daniel Macias <daniel@bender.local>
2021-11-09 12:59:33 -0600

    adding logs

--- commands/reminders.js
+++ commands/reminders.js
@@ -1,6 +1,9 @@
 const discord = require('discord.js');
 const axios = require('axios');
 
+module.exports.run = async (bot, message, args) => {
+    return message.channel.send('All systems are go!')
+};
 
 module.exports.help = {
     name: "reminder"
