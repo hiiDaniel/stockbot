2efa34716ebaacb2f131485b73f26714e10fc1c1
Daniel Macias <daniel@bender.local>
2021-11-08 22:14:21 -0600

    adding logs

--- commands/crypto.js
+++ commands/crypto.js
@@ -17,7 +17,7 @@ module.exports.run = async (bot, message, args) => {
 
             return {
                 cryptoInfo
-            };            
+            };
         }
 
         getData.catch((error) => {
